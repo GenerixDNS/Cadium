2ab93d23f7b2cb23b58fb5d4e99a5f8f6e7f5859
GenerixDNS <Lightmicrash@gmail.com>
2023-01-02 16:09:52 +0100

    add native future impl

--- future-transport/base.cpp
+++ future-transport/base.cpp
@@ -4,3 +4,111 @@
 
 #include "base.h"
 
+class FutureNotFoundException : public std::exception
+{
+private:
+    std::string m_message;
+
+public:
+
+    explicit FutureNotFoundException(const char* message) : m_message(message)
+    {
+    }
+
+    [[nodiscard]]
+    const char* what() const noexcept override { return m_message.c_str(); }
+
+};
+
+static std::vector<std::pair<JPointer, std::future<std::any>>> CACHED_FUTURES;
+
+const std::future<std::any>* GET_BY_UNIQUE(JPointer pointer)
+{
+    for (const auto &item: CACHED_FUTURES)
+    {
+        if (item.first == pointer)
+        {
+            return &item.second;
+        }
+    }
+    throw FutureNotFoundException("A Future with this id couldn't be found!");
+}
+
+bool FLUSH_BY_UNIQUE_ID(JPointer pointer)
+{
+    int target = -1;
+    for (int i = 0; i < CACHED_FUTURES.size(); ++i) {
+        auto& c = CACHED_FUTURES[i];
+        if (c.first == pointer)
+        {
+            target = i;
+            break;
+        }
+    }
+    if (target == -1)
+    {
+        throw FutureNotFoundException("A Future with this id couldn't be found!");
+    } else
+    {
+        CACHED_FUTURES.erase(CACHED_FUTURES.begin() + target);
+        return true;
+    }
+}
+
+JPointer cast(jint& unique_id)
+{
+    if (unique_id != -1)
+    {
+        return static_cast<unsigned int>(unique_id);
+    }
+    return 0;
+}
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    async
+ * Signature: (ILorg/cadium/future/FutureTask;)V
+ */
+JNIEXPORT void JNICALL Java_org_cadium_future_FutureStandardExecutor_async
+        (JNIEnv* environment, jclass, jint unique_id, jobject handler)
+{
+    auto pointer = cast(unique_id);
+    std::future<std::any> future = std::async([]()
+            {
+                return std::any();
+            });
+    CACHED_FUTURES.push_back(std::pair{ pointer, future });
+}
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    collect
+ * Signature: (I)Ljava/lang/Object;
+ */
+JNIEXPORT jobject JNICALL Java_org_cadium_future_FutureStandardExecutor_collect
+        (JNIEnv *, jclass, jint)
+{
+
+}
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    flush
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_org_cadium_future_FutureStandardExecutor_flush
+        (JNIEnv *, jclass, jint)
+{
+
+}
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    wait
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_org_cadium_future_FutureStandardExecutor_wait
+        (JNIEnv *, jclass, jint)
+{
+
+}
--- future-transport/base.h
+++ future-transport/base.h
@@ -1,14 +1,61 @@
 /*
  * Created by Alexander J. on 02.01.2023.
  */
+#include <jni.h>
+#include <future>
+#include <iostream>
+#include <any>
+#include <vector>
+#include <exception>
 
 #ifndef FUTURE_TRANSPORT_BASE_H
 #define FUTURE_TRANSPORT_BASE_H
 
 #endif
 
-#include <future>
-#include <iostream>
+typedef unsigned int JPointer;
 
+/* returns future of the java unique_id */
+const std::future<std::any> * GET_BY_UNIQUE(JPointer pointer);
 
+bool FLUSH_BY_UNIQUE_ID(JPointer pointer);
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    async
+ * Signature: (ILorg/cadium/future/FutureTask;)V
+ */
+JNIEXPORT void JNICALL Java_org_cadium_future_FutureStandardExecutor_async
+        (JNIEnv *, jclass, jint, jobject);
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    collect
+ * Signature: (I)Ljava/lang/Object;
+ */
+JNIEXPORT jobject JNICALL Java_org_cadium_future_FutureStandardExecutor_collect
+        (JNIEnv *, jclass, jint);
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    flush
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_org_cadium_future_FutureStandardExecutor_flush
+        (JNIEnv *, jclass, jint);
+
+/*
+ * Class:     org_cadium_future_FutureStandardExecutor
+ * Method:    wait
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_org_cadium_future_FutureStandardExecutor_wait
+        (JNIEnv *, jclass, jint);
+
+#ifdef __cplusplus
+}
+#endif
